e9eb4abf92f89588621367e60acdf5505775145e
BhΔ∇Δt <107737002+AKABharat@users.noreply.github.com>
2023-11-27 17:10:36 +0530

    Update script.js
    added comments

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  // DOM element variables
   let displayCity = document.querySelector("#displayCity");
   let cityName = document.querySelector("#cityName");
   let SubmitButton = document.querySelector("#submit_button");
-  // let cloud_pct = document.querySelector("#cloud_pct");
   let temp = document.querySelector(".temp");
   let feels_like = document.querySelector("#feels_like");
   let humidity = document.querySelector(".humidity");
@@ -17,18 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let Small_temp = document.querySelector(".Small_temp");
   let year = document.querySelector(".year");
 
-  // let cityName = "";
-  // document.querySelector("button").addEventListener("click", () => {
-  //   cityName = document.querySelector(".searchInput").value;
-
-
-  // });
-
-  // let url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${cityName}`;
-  // let url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=jaipur`;
-
-  // displayCity.innerHTML = "jaipur"
-
+  // RapidAPI headers for the weather API
   const options = {
     method: 'GET',
     headers: {
@@ -36,8 +25,8 @@ document.addEventListener("DOMContentLoaded", () => {
       'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
     }
   };
-  // getWeatherMethod();
 
+  // Function to check for rain based on humidity
   function checkRain(humidity) {
     if (humidity >= 70) {
       return "<h1>🌧️⛈️</h1> High chance of rain and thunderstorms.";
@@ -48,77 +37,58 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  // Function to convert Unix timestamp to 12-hour time format
   function covertingTime(time) {
-    const unixTimestamp = time; // Replace with your actual Unix timestamp
-
-    // Convert Unix timestamp to milliseconds
+    const unixTimestamp = time;
     const timestampInMilliseconds = unixTimestamp * 1000;
-
-    // Create a new Date object using the converted timestamp
     const sunriseDate = new Date(timestampInMilliseconds);
-
-    // Get the individual components of the time
     const hours = sunriseDate.getHours();
     const minutes = sunriseDate.getMinutes();
     const ampm = hours >= 12 ? 'PM' : 'AM';
-
-    // Convert 24-hour time to 12-hour time
     const hours12 = hours % 12 || 12;
-
-    // Format the components into a 12-hour time string (hh:mm AM/PM)
     return `${hours12.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${ampm}`;
-
   }
 
-
+  // Function to update the current year
   function update_year() {
     let time = new Date();
     year.innerHTML = time.getFullYear();
   }
+  
+  // Initial call to update the year
   update_year();
 
+  // Function to get weather information for a given city
   const getWeatherMethod = (cityName) => {
-
     displayCity.innerHTML = cityName;
     fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${cityName}`, options)
       .then(response => response.json())
       .then((result) => {
-        console.log(result);
-
-        // cloud_pct.innerHTML = result.cloud_pct;
+        // Update HTML elements with weather information
         temp.innerHTML = result.temp;
         Small_temp.innerHTML = result.temp;
-        // feels_like.innerHTML = result.feels_like;
         humidity.innerHTML = result.humidity;
         rain_update.innerHTML = checkRain(result.humidity);
         min_temp.innerHTML = result.min_temp;
         wind_degrees.innerHTML = result.wind_degrees;
         max_temp.innerHTML = result.max_temp;
         wind_speed.innerHTML = result.wind_speed;
-        wind_degrees.innerHTML = result.wind_degrees;
         sunrise.innerHTML = covertingTime(result.sunrise);
         sunset.innerHTML = covertingTime(result.sunset);
-        // sunrise.innerHTML = result.sunrise;
-        // sunset.innerHTML = result.sunset;
+
+        // Debugging console logs
         console.log(humidity.innerHTML);
         console.log(rain_update.innerHTML);
-
-
-      }
-      )
+      })
       .catch(err => console.error(err));
   }
 
+  // Event listener for the submit button
   SubmitButton.addEventListener("click", (event) => {
     event.preventDefault();
-    // displayCity.innerHTML = cityName.value;
-
     getWeatherMethod(cityName.value);
   });
 
-
-  getWeatherMethod("jaipur")
-
-
+  // Initial call to get weather information for "jaipur" on page load
+  getWeatherMethod("jaipur");
 });
-
